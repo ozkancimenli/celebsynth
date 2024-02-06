50f8c966b71941e3e9c92e800eeeaa177a357b3b
Özkan Çimenli <ozkan.cimenli@hotmail.com>
2024-02-06 04:26:38 +0300

    first

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Celebsynth',
+  title: 'Celebsynth.ai',
   description: 'Your customized companion.',
   icons: {
     icon: '/app/favicon.png', 
